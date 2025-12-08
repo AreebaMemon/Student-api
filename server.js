f6c2bf1cfdc57eb9d9dbc47c410913ac1b8de55d
Areeba Memon <103986750+AreebaMemon@users.noreply.github.com>
2025-12-08 15:54:43 +0500

    Enhance welcome message with API usage details
    Updated the welcome message to include API usage instructions.

--- server.js
+++ server.js
@@ -17,12 +17,19 @@ const studentRoutes = require("./routes/students");
 app.use("/api/students", studentRoutes);
 
 app.get("/", (req, res) => {
-  res.send("Welcome to PBL API - Express + MongoDB");
+  res.send(`
+    <h1>Simple Node.js REST API for managing students</h1>
+<p> <b>GET</b> /api/students — list all students  </p>
+<p> <b>GET</b> /api/students/:id — get a student by id </p>
+<p> <b>POST</b> api/students — create a student (JSON body) </p>
+<p> <b>PUT</b> api/students/:id — update a student </p>
+<p> <b>DELETE</b> api/students/:id — delete a student <p>`);
 });
 
 app.listen(port, () => {
   console.log(`🚀 Server running on http://localhost:${port}`);
 });
+
 
 
 
